c6e48d45351e38b8e07365d639ec875c2e2a3ff2
Pat Keenan <patkeenan.dev@gmail.com>
2023-06-30 17:48:05 -0400

    format: ran prettier

--- src/lib-server/getWorkspacesAccess.ts
+++ src/lib-server/getWorkspacesAccess.ts
@@ -1,27 +1,32 @@
 import { prisma } from "@/lib-server/db";
 import { clerkClient } from "@clerk/nextjs";
 
-import type { SignedInAuthObject, SignedOutAuthObject } from "@clerk/nextjs/dist/types/server";
+import type {
+  SignedInAuthObject,
+  SignedOutAuthObject,
+} from "@clerk/nextjs/dist/types/server";
 
 // Used to get workspace access info for the current user
-export async function getWorkspacesAccess(auth: SignedInAuthObject | SignedOutAuthObject){
-    if(auth && auth.userId){
-      const userInfo = await clerkClient.users.getUser(auth.userId);
-      const emails = userInfo.emailAddresses.map((email) => email.emailAddress)
-      if(emails.length > 0){
-        return await prisma.workspaceUser.findMany({
-          where: {
-            email: {
-              in: emails
-            }
+export async function getWorkspacesAccess(
+  auth: SignedInAuthObject | SignedOutAuthObject
+) {
+  if (auth && auth.userId) {
+    const userInfo = await clerkClient.users.getUser(auth.userId);
+    const emails = userInfo.emailAddresses.map((email) => email.emailAddress);
+    if (emails.length > 0) {
+      return await prisma.workspaceUser.findMany({
+        where: {
+          email: {
+            in: emails,
           },
-           select: {
-            workspaceId: true,
-            role: true,
-            status: true
-           }
-        })
-      }
+        },
+        select: {
+          workspaceId: true,
+          role: true,
+          status: true,
+        },
+      });
     }
-    return undefined
   }
+  return undefined;
+}
